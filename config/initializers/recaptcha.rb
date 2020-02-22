00f22ee8e3109641e5d5bf9c5f01cb799ecd82d2
Marcos Riveros <mriveros.py@gmail.com>
2020-02-22 16:23:08 -0300

    config/initializers/recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 Recaptcha.configure do |config|
 
-  config.site_key  = '6Lc-0PcSAAAAAEgTs-_EdpQU-LHIPx0bpRjnkMbR'
-  config.secret_key = '6Lc-0PcSAAAAANs5JkrSwG5JEtw5BhEiv4OfnEYL'
+  #config.site_key  = '6Lc-0PcSAAAAAEgTs-_EdpQU-LHIPx0bpRjnkMbR'
+  #config.secret_key = '6Lc-0PcSAAAAANs5JkrSwG5JEtw5BhEiv4OfnEYL'
   #config.public_key  = '6Lc-0PcSAAAAAEgTs-_EdpQU-LHIPx0bpRjnkMbR'
   #config.private_key = '6Lc-0PcSAAAAANs5JkrSwG5JEtw5BhEiv4OfnEYL'
   # Uncomment the following line if you are using a proxy server:
