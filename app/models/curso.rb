cbe688c0dcee519b91951d77b3323b5276c20092
Marcos Riveros <mriveros.py@gmail.com>
2020-03-31 19:53:03 -0400

    adding details for patients

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,7 +1,7 @@
 class Curso < ActiveRecord::Base
   
   scope :orden_id, -> { order('id')}
-  scope :basica, -> {where('id in (1,2,3,4,5,6,7,8,9,10,11,12,13)')}
+  scope :basica, -> {where('id in (9,20,21,22,23,24,25,26,27,28,29,30,31,32,33)')}
   scope :media, -> {where('id in (14,15,16,25,26,27)')}
  
 end
