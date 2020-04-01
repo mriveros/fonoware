35e411fd2e343879bd3e9455a71538bed5beaa4e
Marcos Riveros <mriveros.py@gmail.com>
2020-04-01 18:10:51 -0400

    finishing details

--- app/models/paciente_detalle_fono.rb
+++ app/models/paciente_detalle_fono.rb
@@ -3,7 +3,7 @@ class PacienteDetalleFono < ActiveRecord::Base
   self.table_name="pacientes_detalles_fono"
   self.primary_key="id"
   
-  attr_accessible :id, :paciente_id, :antecedentes_personales, :antecedentes_familiares, :antecedentes_otros, :antecedentes_habitos, :diagnostico_anterior, :sindrome,
+  attr_accessible :id, :paciente_id, :antecedentes_personales, :antecedentes_familiares, :antecedentes_otros, :antecedentes_habitos, :sindrome,
   :sindrome_especificar, :transtornos, :transtorno_especificar, :enfermedad, :enfermedad_especificar, 
   :escolaridad_grado_id, :escolaridad_tiempo, :escolaridad_institucion, :datos_derivacion
   
