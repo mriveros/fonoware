5bf7c60486a073e0786de83707034d6072e503fb
Marcos Riveros <mriveros.py@gmail.com>
2020-04-01 18:18:25 -0400

    adding audit

--- app/controllers/pacientes_controller.rb
+++ app/controllers/pacientes_controller.rb
@@ -287,6 +287,7 @@ skip_before_action :verify_authenticity_token
 
     if @paciente_detalle_fono.present?
 
+      auditoria_id = auditoria_antes("actualizar datos de detalles de pacientes", "pacientes_detalles_fono", @paciente_detalle_fono)
       @paciente_detalle_fono.antecedentes_personales = params[:antecedentes_personales]
       @paciente_detalle_fono.antecedentes_familiares = params[:antecedentes_familiares]
       @paciente_detalle_fono.antecedentes_otros = params[:antecedentes_otros]
@@ -305,7 +306,7 @@ skip_before_action :verify_authenticity_token
       if @paciente_detalle_fono.save
 
         @guardado_ok = true
-        auditoria_nueva("registrar paciente detalle fono", "pacientes_detalles_fono", @paciente_detalle_fono)
+        auditoria_despues(@paciente_detalle_fono, auditoria_id)
 
       end
 
