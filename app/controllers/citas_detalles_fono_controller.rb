80613c868655574d9848e92df438aa87d311f62c
Marcos Riveros <mriveros.py@gmail.com>
2020-04-12 21:45:38 -0400

    On branch master  Changes committed: 	modified:   app/controllers/citas_detalles_fono_controller.rb 	modified:   app/views/citas_detalles_fono/_cita_detalle_fono.html.erb 	new file:   app/views/citas_detalles_fono/guardar_cita_detalle_fono.js.erb 	modified:   app/views/pacientes/_paciente_detalle_fono.html.erb

--- app/controllers/citas_detalles_fono_controller.rb
+++ app/controllers/citas_detalles_fono_controller.rb
@@ -9,7 +9,8 @@ before_filter :require_usuario
   def cita_detalle_fono
 
     @cita = Cita.where("id = ?", params[:cita_id]).first
-    @cita_detalle_fono = nil
+    @cita_detalle_fono = CitaDetalleFono.where("cita_id = ?", params[:cita_id]).first
+    
     respond_to do |f|
 
       f.js
@@ -17,16 +18,57 @@ before_filter :require_usuario
     end
 
   end
+
 
   def guardar_cita_detalle_fono
 
+      @valido = true
+      guardado_ok = false
 
-    respond_to do |f|
+      @cita = Cita.where("id = ?", params[:cita_id]).first
 
-      f.js
+      @cita_detalle_fono = CitaDetalleFono.where("cita_id = ?", params[:cita_id]).first
+
+      if @cita_detalle_fono.present?
+
+        auditoria_id = auditoria_antes("actualizar datos de la cita detalle fono", "citas_detalles_fono", @cita_detalle_fono)
+        @cita_detalle_fono.cita_id = params[:cita_id]
+        @cita_detalle_fono.objetivo = params[:objetivo]
+        @cita_detalle_fono.estrategia = params[:estrategia]
+        @cita_detalle_fono.resultado = params[:resultado]
+        @cita_detalle_fono.observacion = params[:observacion]
+        
+        if @cita_detalle_fono.save
+
+          @guardado_ok = true
+          auditoria_despues(@cita_detalle_fono, auditoria_id)
+
+        end
+
+      else
+
+        @cita_detalle_fono = CitaDetalleFono.new
+        @cita_detalle_fono.cita_id = params[:cita_id]
+        @cita_detalle_fono.objetivo = params[:objetivo]
+        @cita_detalle_fono.estrategia = params[:estrategia]
+        @cita_detalle_fono.resultado = params[:resultado]
+        @cita_detalle_fono.observacion = params[:observacion]
+        
+        if @cita_detalle_fono.save
+
+          @guardado_ok = true
+          auditoria_nueva("registrar detalles de la cita fonoaudiologica", "citas_detalles_fono", @cita_detalle_fono)
+
+        end
+
+      end
+
+      respond_to do |f|
+
+        f.js
+
+      end
 
     end
-
-  end
 
 end
