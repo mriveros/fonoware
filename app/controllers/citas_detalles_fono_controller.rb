c05c62015965aed638274ba3db5724d81ae16789
Marcos Riveros <mriveros.py@gmail.com>
2020-04-12 22:28:31 -0400

    On branch master  Changes committed: 	modified:   app/controllers/citas_detalles_fono_controller.rb 	new file:   app/models/resolucion.rb adding files for resolucion, archivos(files) to attach date details

--- app/controllers/citas_detalles_fono_controller.rb
+++ app/controllers/citas_detalles_fono_controller.rb
@@ -71,4 +71,179 @@ before_filter :require_usuario
 
     end
 
+  def emitir_resolucion
+
+    @cita_detalle_fono = CitaDetalleFono.where("id = ?", params[:cita_id]).first
+    @resolucion = Resolucion.new
+
+    respond_to do |f|
+
+      f.js
+
+    end    
+
+  end
+
+  def guardar_resolucion
+
+    @valido = true    
+    @msg = ""
+    
+    @resolucion = Resolucion.new(params[:resolucion])
+
+    unless @resolucion.numero.present?
+
+      @valido = false
+      @msg += "Debe indicar el numero de la resolucion. \n"
+
+    end
+
+    unless @resolucion.descripcion.present?
+
+      @valido = false
+      @msg += "Debe ingresar una descripcion a la resolucion. \n"
+
+    end
+
+    if @resolucion.fecha_emision.present?
+
+      fecha_emision = Date.parse(@resolucion.fecha_emision.to_s)
+      
+      fecha_actual =  Time.now     
+     
+      if fecha_emision > fecha_actual
+
+        @valido = false
+        @msg += "La fecha de la resolucion debe ser menor o igual a la fecha actual.\n"
+
+      else
+
+      end
+
+    else
+
+      @valido = false
+      @msg += "Seleccione la fecha de resolucion.\n"
+
+    end   
+
+    if @valido
+
+     
+      @cita_detalle_fono = CitaDetalleFono.where("id = ? and resolucion_id is null", params[:resolucion][:cita_detalle_id]).first
+
+      if @cita_detalle.present?
+
+        @resolucion.tipo_resolucion_id = PARAMETRO[:archivo_fonoaudiologico]
+        
+                              
+        if @resolucion.save
+      
+          auditoria_nueva("registrar archivo de citas detalles fono", "resolucion", @resolucion)
+          
+          auditoria_id = auditoria_antes("Agregar archivo en citas detalles fono", 'citas_detalles_fono', @cita_detalle_fono)
+
+          @cita_detalle_fono.resolucion_id = @resolucion.id
+            
+          if @cita_detalle_fono.save
+
+            auditoria_despues(@cita_detalle_fono, auditoria_id)
+
+          end          
+                    
+        end
+ 
+      else
+         
+        @valido = false
+        @msg += "La Cita ya posee un archivo o no se encontraron datos del proceso. Favor Verifique. \n"
+
+      end
+
+    end 
+
+  rescue Exception => exc  
+    
+    # dispone el mensaje de error 
+    #puts "Aqui si muestra el error ".concat(exc.message) 
+       
+    if exc.present?   
+
+      @excep = exc.message.split(':')    
+      @msg = @excep[3].concat(" "+@excep[4])
+      @valido = false  
+
+    end
+
+    respond_to do |f|
+
+      f.js
+
+    end     
+
+  end
+
+  def adjuntar_resolucion
+
+    @cita_detalle_fono = CitaDetalleFono.where("id = ?", params[:cita_id]).first
+    @resolucion = Resolucion.find(params[:resolucion_id])
+
+    respond_to do |f|
+
+      f.js
+
+    end    
+  end 
+
+  def guardar_resolucion_adjunta
+
+    @valido = true    
+    @msg = ""
+    
+    @cita_detalle_fono = CitaDetalleFono.where("id = ?", params[:cita_id]).first
+    
+    unless params[:resolucion][:data].present?
+
+      @valido = false
+      @msg += "Debe adjuntar un documento PDF."
+
+    end    
+   
+    if @valido
+
+      @resolucion = Resolucion.find(params[:resolucion][:id])
+      auditoria_id = auditoria_antes("adjuntar archivo de citas detalles fono", "resolucion", @resolucion)
+      @resolucion.update_attributes(params[:resolucion])
+
+                              
+      if @resolucion.save
+        
+        auditoria_despues(@resolucion, auditoria_id)         
+        
+      end
+
+    end
+
+    rescue Exception => exc  
+      # dispone el mensaje de error 
+      #puts "Aqui si muestra el error ".concat(exc.message) 
+         
+      if exc.present?    
+
+        @excep = exc.message.split(':')    
+        @msg = @excep[3].concat(" "+@excep[4])
+        @valido = false  
+
+      end
+
+      respond_to do |f|
+
+        f.js
+
+      end     
+        
+  end
+
+
+
 end
---
+++ app/models/resolucion.rb
@@ -0,0 +1,13 @@
+class Resolucion < ActiveRecord::Base
+
+  self.table_name="resoluciones"
+  self.primary_key = 'id'
+ 
+  attr_accessible :id, :numero, :descripcion, :fecha_emision, :tipo_resolucion_id, :data
+  belongs_to :tipo_resolucion
+  #has_many :v_notas_basicas_detalles, :class_name => 'VNotaBasicaDetalle', :foreign_key => 'resolucion_id'
+  scope :orden_id, -> {order("id")}
+  has_attached_file :data
+  validates_attachment_content_type :data, :content_type => ['application/pdf', 'application/binary']
+ 
+end
